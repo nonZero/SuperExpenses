0770940d9d109ac023732fdfa696b3d486c30e10
Udi Oron <udioron@gmail.com>
2024-03-14 15:03:13 +0200

    simple urls

---
+++ super_expenses/urls.py
@@ -0,0 +1,36 @@
+"""
+URL configuration for super_expenses project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+import random
+
+from django.contrib import admin
+from django.http import HttpRequest, HttpResponse
+from django.urls import path
+
+
+def my_view(request: HttpRequest):
+    return HttpResponse("Hello world!!")
+
+
+def random_number(request: HttpRequest):
+    return HttpResponse(f"The magic number is {random.randint(0, 100)}!!!!")
+
+
+urlpatterns = [
+    path("", my_view),
+    path("rnd/", random_number),
+    path("admin/", admin.site.urls),
+]
